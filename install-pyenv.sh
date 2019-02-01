3ea0a08a1eb9f7014c0878069f417118769807ae
Niko Steinhoff <niko.steinhoff@gmail.com>
2019-02-01 08:58:59 +0100

    Fix pyenv install script

--- install-pyenv.sh
+++ install-pyenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ ! -d ~/.pyenv ]]; then
     echo "Installing Pyenv to ~/.pyenv"
-    git clone https://github.com/pyenv/pyenv.git "~/.pyenv"
+    git clone https://github.com/pyenv/pyenv.git "$HOME/.pyenv"
 fi
 
 echo "Installing Python dependencies"
