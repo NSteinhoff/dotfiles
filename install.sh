c018d06f8c03602d28a281c012f8a0e5a13406e0
Niko Steinhoff <niko.steinhoff@gmail.com>
2019-04-26 08:44:05 +0200

    Add config file installation and backup
    For files that live in '~/.config' (where the belong!!!).
    
    This commit also include a basic config for the Alacritty terminal
    emulator.

--- install.sh
+++ install.sh
@@ -4,13 +4,7 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
-########## Variables
-
-dir=~/dotfiles                    # dotfiles directory
-bkpdir=~/dotfiles_bkp             # old dotfiles backup directory
-files=(inputrc vimrc vim git_template screenrc gitconfig gitignore tmux.conf zshrc bash_aliases crawlrc ctags ctags.d scalafmt.conf)
-
-##########
+########## ZSH
 
 # install zsh
 if false; then
@@ -21,6 +15,12 @@ if false; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     tput sgr0
 fi
+
+########## Dotfiles in HOME
+
+dir=~/dotfiles                    # dotfiles directory
+bkpdir=~/.dotfiles_bkp
+files=(inputrc vimrc vim git_template screenrc gitconfig gitignore tmux.conf zshrc bash_aliases crawlrc ctags ctags.d scalafmt.conf)
 
 # create dotfiles_old in homedir
 echo "Creating $bkpdir for backup of any existing dotfiles in ~ ..."
@@ -41,7 +41,7 @@ for file in ${files[*]}; do
     if [ -f ~/.$file ] || [ -d ~/.$file ]; then
         tput setaf 3
         echo -n "Moving existing file; "
-        mv -v ~/.$file $bkpdir/.$file
+        mv -v ~/.$file $bkpdir/$file
         tput sgr0
     fi
     tput setaf 2
@@ -49,6 +49,40 @@ for file in ${files[*]}; do
     tput sgr0
     ln -v -s $dir/$file ~/.$file
 done
+
+########## Configurations in ~/.config
+
+cfgdir=$dir/config
+cfgbkpdir=~/.config/backup_$(date +%F-%T)
+files=(alacritty/alacritty.yml)
+
+# create dotfiles_old in homedir
+
+# change to the dotfiles directory
+echo "Changing to the $cfgdir directory ..."
+cd $cfgdir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+for file in ${files[*]}; do
+    if [ -L ~/.config/$file ]; then
+        tput setaf 3
+        echo -n "Removing old symlink; "
+        rm ~/.config/$file
+        tput sgr0
+    fi
+    if [ -f ~/.config/$file ] || [ -d ~/.config/$file ]; then
+        tput setaf 3
+        echo -n "Backing-up existing file; "
+        mkdir -p $cfgbkpdir/${file%/*}
+        mv -v ~/.config/$file $cfgbkpdir/$file
+        tput sgr0
+    fi
+    tput setaf 2
+    echo -n "Creating symlink: "
+    tput sgr0
+    ln -v -s $cfgdir/$file ~/.config/$file
+done
+
 
 # Init and update the plugins tracked as submodules
 git submodule update --init --recursive
