6f61fd654ed0c94ca9c95a6959e0994e56e2d677
Niko Steinhoff <niko.steinhoff@gmail.com>
2020-12-05 20:24:26 +0100

    Make LSP very silent

--- stow/nvim/.config/nvim/pack/personal/opt/my-lsp/lua/lsp.lua
+++ stow/nvim/.config/nvim/pack/personal/opt/my-lsp/lua/lsp.lua
@@ -115,8 +115,8 @@ end
 -- Handlers
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
-        signs = true,
-        underline = true,
+        signs = false,
+        underline = false,
         virtual_text = false,
         update_in_insert = false,
     }
