c2db45cc74970e710bbbd200871aa54fcf74b24f
Niko Steinhoff <niko.steinhoff@gmail.com>
2021-02-07 08:32:13 +0100

    Disable tree-sitter indentation

--- stow/nvim/.config/nvim/pack/personal/opt/my-treesitter/lua/my_treesitter.lua
+++ stow/nvim/.config/nvim/pack/personal/opt/my-treesitter/lua/my_treesitter.lua
@@ -3,7 +3,7 @@ vim.cmd('packadd nvim-treesitter')
 require'nvim-treesitter.configs'.setup {
     ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     indent = {
-        enable = true,
+        enable = false,
     },
     highlight = {
         enable = true,
