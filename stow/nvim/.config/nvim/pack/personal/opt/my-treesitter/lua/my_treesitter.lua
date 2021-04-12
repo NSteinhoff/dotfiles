e5160f2bcc73eca1d29479df1c698e200a97386a
Niko Steinhoff <niko.steinhoff@gmail.com>
2021-04-12 08:27:02 +0200

    Enable treesitter
    Let's see if it has become more stable.

--- stow/nvim/.config/nvim/pack/personal/opt/my-treesitter/lua/my_treesitter.lua
+++ stow/nvim/.config/nvim/pack/personal/opt/my-treesitter/lua/my_treesitter.lua
@@ -3,7 +3,7 @@ vim.cmd('packadd nvim-treesitter')
 require'nvim-treesitter.configs'.setup {
     ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     indent = {
-        enable = false,
+        enable = true,
     },
     highlight = {
         enable = true,
