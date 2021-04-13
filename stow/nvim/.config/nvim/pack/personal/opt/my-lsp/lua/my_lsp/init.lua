ffc61b4047d67a17252e2ce5103c07b89214c93a
Niko Steinhoff <niko.steinhoff@gmail.com>
2021-04-13 15:38:34 +0200

    Add nvim-lsp-ts-utils
    Add tsserver specific LSP utilities.

--- stow/nvim/.config/nvim/pack/personal/opt/my-lsp/lua/my_lsp/init.lua
+++ stow/nvim/.config/nvim/pack/personal/opt/my-lsp/lua/my_lsp/init.lua
@@ -18,7 +18,26 @@ for _, server in ipairs(servers) do
 end
 
 lspconfig["tsserver"].setup({
-    on_attach = on_attach,
+    on_attach = function(client, bufnr, ...)
+        vim.cmd("packadd nvim-lsp-ts-utils")
+        require("nvim-lsp-ts-utils").setup {
+            disable_commands = true,
+            enable_import_on_completion = false,
+            import_on_completion_timeout = 5000
+        }
+
+        vim.cmd([[command! -bar -buffer LspTsOrganize lua require'nvim-lsp-ts-utils'.organize_imports()]])
+        vim.cmd([[command! -bar -buffer LspTsFixCurrent lua require'nvim-lsp-ts-utils'.fix_current()]])
+        vim.cmd([[command! -bar -buffer LspTsRenameFile lua require'nvim-lsp-ts-utils'.rename_file()]])
+        vim.cmd([[command! -bar -buffer LspTsImportAll lua require'nvim-lsp-ts-utils'.import_all()]])
+
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "dcO", "<CMD>LspTsOrganize<CR>", {silent = true, noremap = true})
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "dcA", "<CMD>LspTsFixCurrent<CR>", {silent = true, noremap = true})
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "dcR", "<CMD>LspTsRenameFile<CR>", {silent = true, noremap = true})
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "dcI", "<CMD>LspTsImportAll<CR>", {silent = true, noremap = true})
+
+        on_attach(client, bufnr, ...)
+    end,
     root_dir = function(fname)
         -- Prefer the repository root for typescript
         -- NOTE:
