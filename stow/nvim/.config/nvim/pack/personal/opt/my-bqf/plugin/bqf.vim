b9c7e0d7cb3bd6c7858ed97b7a4a9576d3ad2032
Niko Steinhoff <niko.steinhoff@gmail.com>
2021-04-25 17:10:11 +0200

    Remove 'bqf' plugin

--- stow/nvim/.config/nvim/pack/personal/opt/my-bqf/plugin/bqf.vim
+++
@@ -1,3 +0,0 @@
-let g:loaded_bqf = 1
-
-lua require('my_bqf')
--- stow/nvim/.config/nvim/pack/personal/opt/my-treesitter/lua/my_treesitter.lua
+++ stow/nvim/.config/nvim/pack/personal/opt/my-treesitter/lua/my_treesitter.lua
@@ -1,7 +1,7 @@
 vim.cmd("packadd nvim-treesitter")
 
 require("nvim-treesitter.configs").setup({
-    ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     indent = {
         enable = true,
     },
