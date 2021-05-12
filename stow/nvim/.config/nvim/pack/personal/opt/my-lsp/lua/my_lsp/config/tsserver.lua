7146be6d3c72cefab49eb41aa51a96b543d07db3
Niko Steinhoff <niko.steinhoff@gmail.com>
2021-05-12 08:45:21 +0200

    Handle missing plugin when attaching tsserver

--- stow/nvim/.config/nvim/pack/personal/opt/my-lsp/lua/my_lsp/config/tsserver.lua
+++ stow/nvim/.config/nvim/pack/personal/opt/my-lsp/lua/my_lsp/config/tsserver.lua
@@ -1,26 +1,34 @@
 local lspconfig = require("lspconfig")
 
+local function setup_ts_utils()
+    vim.cmd("packadd nvim-lsp-ts-utils")
+    require("nvim-lsp-ts-utils").setup({
+        disable_commands = true,
+        enable_import_on_completion = false,
+        import_on_completion_timeout = 5000,
+    })
+
+    vim.cmd([[command! -bar -buffer LspTsOrganize lua require'nvim-lsp-ts-utils'.organize_imports()]])
+    vim.cmd([[command! -bar -buffer LspTsFixCurrent lua require'nvim-lsp-ts-utils'.fix_current()]])
+    vim.cmd([[command! -bar -buffer LspTsRenameFile lua require'nvim-lsp-ts-utils'.rename_file()]])
+    vim.cmd([[command! -bar -buffer LspTsImportAll lua require'nvim-lsp-ts-utils'.import_all()]])
+
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "dcO", "<CMD>LspTsOrganize<CR>", { silent = true, noremap = true })
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "dcA", "<CMD>LspTsFixCurrent<CR>", { silent = true, noremap = true })
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "dcR", "<CMD>LspTsRenameFile<CR>", { silent = true, noremap = true })
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "dcI", "<CMD>LspTsImportAll<CR>", { silent = true, noremap = true })
+end
+
 return function(config)
     local override = {
         autostart = true,
 
         on_attach = function(...)
-            vim.cmd("packadd nvim-lsp-ts-utils")
-            require("nvim-lsp-ts-utils").setup({
-                disable_commands = true,
-                enable_import_on_completion = false,
-                import_on_completion_timeout = 5000,
-            })
+            local success, result = pcall(setup_ts_utils)
 
-            vim.cmd([[command! -bar -buffer LspTsOrganize lua require'nvim-lsp-ts-utils'.organize_imports()]])
-            vim.cmd([[command! -bar -buffer LspTsFixCurrent lua require'nvim-lsp-ts-utils'.fix_current()]])
-            vim.cmd([[command! -bar -buffer LspTsRenameFile lua require'nvim-lsp-ts-utils'.rename_file()]])
-            vim.cmd([[command! -bar -buffer LspTsImportAll lua require'nvim-lsp-ts-utils'.import_all()]])
-
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "dcO", "<CMD>LspTsOrganize<CR>", { silent = true, noremap = true })
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "dcA", "<CMD>LspTsFixCurrent<CR>", { silent = true, noremap = true })
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "dcR", "<CMD>LspTsRenameFile<CR>", { silent = true, noremap = true })
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "dcI", "<CMD>LspTsImportAll<CR>", { silent = true, noremap = true })
+            if not success then
+                print("Unable to setup 'nvim-lsp-ts-utils': "..result)
+            end
 
             config.on_attach(...)
         end,
@@ -39,5 +47,5 @@ return function(config)
         end,
     }
 
-    lspconfig["tsserver"].setup(vim.tbl_extend('keep', override, config))
+    lspconfig["tsserver"].setup(vim.tbl_extend("keep", override, config))
 end
