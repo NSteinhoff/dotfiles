a625e4652179a2393aad1dc4f8592c4d9bb73336
Niko Steinhoff <niko.steinhoff@gmail.com>
2021-07-17 13:39:59 +0200

    Remove nvim-lsp-ts-utils

--- stow/nvim/.config/nvim/pack/personal/opt/my-lsp/lua/my_lsp/config/tsserver.lua
+++ stow/nvim/.config/nvim/pack/personal/opt/my-lsp/lua/my_lsp/config/tsserver.lua
@@ -1,37 +1,8 @@
 local lspconfig = require("lspconfig")
 
-local function setup_ts_utils()
-    vim.cmd("packadd nvim-lsp-ts-utils")
-    require("nvim-lsp-ts-utils").setup({
-        disable_commands = true,
-        enable_import_on_completion = false,
-        import_on_completion_timeout = 5000,
-    })
-
-    vim.cmd([[command! -bar -buffer LspTsOrganize lua require'nvim-lsp-ts-utils'.organize_imports()]])
-    vim.cmd([[command! -bar -buffer LspTsFixCurrent lua require'nvim-lsp-ts-utils'.fix_current()]])
-    vim.cmd([[command! -bar -buffer LspTsRenameFile lua require'nvim-lsp-ts-utils'.rename_file()]])
-    vim.cmd([[command! -bar -buffer LspTsImportAll lua require'nvim-lsp-ts-utils'.import_all()]])
-
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "dcO", "<CMD>LspTsOrganize<CR>", { silent = true, noremap = true })
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "dcA", "<CMD>LspTsFixCurrent<CR>", { silent = true, noremap = true })
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "dcR", "<CMD>LspTsRenameFile<CR>", { silent = true, noremap = true })
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "dcI", "<CMD>LspTsImportAll<CR>", { silent = true, noremap = true })
-end
-
 return function(config)
     local override = {
         autostart = true,
-
-        on_attach = function(...)
-            -- local success, result = pcall(setup_ts_utils)
-
-            -- if not success then
-            --     print("Unable to setup 'nvim-lsp-ts-utils': "..result)
-            -- end
-
-            config.on_attach(...)
-        end,
 
         root_dir = function(fname)
             -- Prefer the repository root for typescript
