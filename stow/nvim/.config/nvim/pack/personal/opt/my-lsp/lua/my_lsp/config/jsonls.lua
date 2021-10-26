b6e3048bb4b8a53d8f7b1a6b88b8425b375f0b23
Niko Steinhoff <niko.steinhoff@gmail.com>
2021-10-26 09:31:51 +0200

    Add plugin to access schemastore catalogue

--- stow/nvim/.config/nvim/pack/personal/opt/my-lsp/lua/my_lsp/config/jsonls.lua
+++ stow/nvim/.config/nvim/pack/personal/opt/my-lsp/lua/my_lsp/config/jsonls.lua
@@ -1,28 +1,13 @@
+vim.cmd([[packadd nvim-schema-store]])
+
 local lspconfig = require("lspconfig")
-
-local schemas = {
-    { "package", { "package.json" } },
-    { "prettierrc", { ".prettierrc.json" } },
-    { "tsconfig", { "tsconfig.json", "tsconfig.settings.json" } },
-}
-
-local function get_schemas()
-    local res = {}
-    for _, schema in ipairs(schemas) do
-        table.insert(res, {
-            fileMatch = schema[2],
-            url = "https://json.schemastore.org/".. schema[1]
-        })
-    end
-    return res
-end
 
 return function(config)
     local override = {
         autostart = false,
         settings = {
             json = {
-                schemas = get_schemas(),
+                schemas = require("schemastore").json.schemas(),
             },
         },
     }
