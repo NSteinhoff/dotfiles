d3915987eb45a4e79bfa52971afb502eeaacb94a
Niko Steinhoff <niko.steinhoff@pm.me>
2022-07-22 08:05:40 +0200

    Disable LSP tagfunc

--- stow/nvim/.config/nvim/pack/personal/opt/my-lsp/lua/my_lsp/options.lua
+++ stow/nvim/.config/nvim/pack/personal/opt/my-lsp/lua/my_lsp/options.lua
@@ -1,5 +1,9 @@
 local function on_attach()
     vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
+    -- 'tagfunc' is now set by default. Disable that!
+    if vim.bo.tagfunc == "v:lua.vim.lsp.tagfunc" then
+        vim.bo.tagfunc = ""
+    end
 end
 
 local function on_detach()
