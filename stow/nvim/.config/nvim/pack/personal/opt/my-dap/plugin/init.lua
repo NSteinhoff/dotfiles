86805e11d50c9933f8bc846a0aa0c7f54b0155f2
Niko Steinhoff <niko.steinhoff@pm.me>
2022-10-01 11:11:36 +0200

    Add basic DAP config

---
+++ stow/nvim/.config/nvim/pack/personal/opt/my-dap/plugin/init.lua
@@ -0,0 +1,85 @@
+vim.cmd([[packadd nvim-dap]])
+local dap = require("dap")
+local ui = require("dap.ui.widgets")
+
+local opts = { buffer = false, silent = false }
+local maps = {
+    ["n"] = {
+        ["dK"] = ui.hover,
+        ["<leader>dc"] = dap.continue,
+        ["<leader>ds"] = dap.step_into,
+        ["<leader>dn"] = dap.step_over,
+        ["<leader>df"] = dap.step_out,
+        ["<leader>db"] = dap.toggle_breakpoint,
+        ["<leader>dB"] = function()
+            dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+        end,
+        ["<leader>dB"] = function()
+            dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+        end,
+        ["<leader>dr"] = dap.repl.open,
+        ["<leader>dd"] = dap.run_last,
+        ["<leader>dt"] = dap.terminate,
+        ["<leader>dl"] = function()
+            dap.list_breakpoints()
+            vim.cmd.cwindow()
+        end,
+        ["<leader>dL"] = dap.clear_breakpoints,
+        ["<leader>dS"] = function()
+            print(dap.status())
+        end,
+    },
+}
+
+function on_attach(client)
+    for mode, map in pairs(maps) do
+        for lhs, rhs in pairs(map) do
+            vim.keymap.set(mode, lhs, rhs, opts)
+        end
+    end
+end
+
+function on_detach()
+    for mode, map in pairs(maps) do
+        for lhs, _ in pairs(map) do
+            vim.keymap.del(mode, lhs, opts)
+        end
+    end
+end
+
+dap.adapters.lldb = {
+    type = "executable",
+    command = "/opt/homebrew/opt/llvm/bin/lldb-vscode", -- adjust as needed, must be absolute path
+    name = "lldb",
+}
+
+local dap = require("dap")
+dap.configurations.c = {
+    {
+        name = "Launch",
+        type = "lldb",
+        request = "launch",
+        program = function()
+            return vim.fn.input(
+                "Path to executable: ",
+                vim.fn.getcwd() .. "/",
+                "file"
+            )
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+        args = {},
+        env = function()
+            local variables = {}
+            for k, v in pairs(vim.fn.environ()) do
+                table.insert(variables, string.format("%s=%s", k, v))
+            end
+            return variables
+        end,
+    },
+}
+
+dap.configurations.rust = dap.configurations.c
+dap.configurations.cpp = dap.configurations.c
+
+on_attach()
